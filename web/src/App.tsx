a923152d89f3f1f6542fad52173922ca1b2de641
Rafael Matoso <rafaelmatosog@gmail.com>
2020-08-03 22:32:13 -0300

    Página Landing criada com algumas configurações

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
+import { Landing } from './pages/Landing';
+
 import './assets/styles/global.css';
 
 function App(): JSX.Element {
   return (
     <div className="App">
-      <h1>Hello Proffy</h1>
-      <button type="button">Clique em mim</button>
+      <Landing />
     </div>
   );
 }
---
+++ web/src/pages/Landing/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Landing: React.FC = () => {
+  return (
+    <div id="page-landing">
+      <div id="page-landing-content" className="container">
+        <img src="" alt="" />
+      </div>
+    </div>
+  );
+};
