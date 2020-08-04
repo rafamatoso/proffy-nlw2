846061c48d61f79903f2c5be65b77cd732b713e3
Rafael Matoso <rafaelmatosog@gmail.com>
2020-08-04 20:17:38 -0300

    Template inicial do Componente TeacherList

--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -1,5 +1,23 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import { images } from '../../assets/images';
+import { icons } from '../../assets/images/icons';
 
 export const TeacherList: React.FC = () => {
-  return <div>Teacher List</div>;
+  return (
+    <div id="page-teacher-list" className="container">
+      <header className="page-header">
+        <div className="top-bar-container">
+          <Link to="/">
+            <img src={icons.back} alt="Voltar" />
+          </Link>
+          <img src={images.logo} alt="Proffy" />
+        </div>
+        <div className="header-content">
+          <strong>Estes são os proffys disponíveis</strong>
+        </div>
+      </header>
+    </div>
+  );
 };
