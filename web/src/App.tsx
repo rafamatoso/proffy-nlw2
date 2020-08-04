fec2528672e434aaa3cf3f1ed69c5406870a9ce9
Rafael Matoso <rafaelmatosog@gmail.com>
2020-08-03 22:19:26 -0300

    Configurações no estilo global, adição de fontes externas e algumas mudanças no App.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
+import './assets/styles/global.css';
+
 function App(): JSX.Element {
   return (
     <div className="App">
       <h1>Hello Proffy</h1>
+      <button type="button">Clique em mim</button>
     </div>
   );
 }
