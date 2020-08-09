c6538eeb55c030b3c285a5d354e4105f42a31cdb
Rafael Matoso <rafaelmatosog@gmail.com>
2020-08-09 13:40:20 -0300

    Finalizada a View GiveClasses com estilização

--- mobile/src/views/GiveClasses/index.tsx
+++ mobile/src/views/GiveClasses/index.tsx
@@ -1,10 +1,29 @@
+import { useNavigation } from '@react-navigation/native';
 import React from 'react';
-import { Text, View } from 'react-native';
+import { View, ImageBackground, Text } from 'react-native';
+import { RectButton } from 'react-native-gesture-handler';
+
+import giveClassesBgImage from '../../assets/images/give-classes-background.png';
+import { styles } from './styles';
 
 export const GiveClasses = () => {
+  const { goBack } = useNavigation();
+
+  const handleNavigateBack = () => {
+    goBack();
+  };
+
   return (
-    <View>
-      <Text>Give Classes</Text>
+    <View style={styles.container}>
+      <ImageBackground resizeMode="contain" source={giveClassesBgImage} style={styles.content}>
+        <Text style={styles.title}>Quer ser um Proffy?</Text>
+        <Text style={styles.description}>
+          Para começar, você precisa se cadastrar como professor na nossa plataforma
+        </Text>
+      </ImageBackground>
+      <RectButton style={styles.okButton} onPress={handleNavigateBack}>
+        <Text style={styles.okButtonText}>Tudo bem</Text>
+      </RectButton>
     </View>
   );
 };
