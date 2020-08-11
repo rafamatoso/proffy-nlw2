1d7f915e876f8eded2d352eae14a11b10438e76e
Rafael Matoso <rafaelmatosog@gmail.com>
2020-08-10 21:51:36 -0300

    Configurações do connection.ts atualizadas

--- server/src/database/connection.ts
+++ server/src/database/connection.ts
@@ -2,9 +2,12 @@ import knex from 'knex';
 import path from 'path';
 
 const db = knex({
-  client: 'sqlite3',
+  client: 'pg',
   connection: {
-    filename: path.resolve(__dirname, 'database.sqlite'),
+    host: 'localhost',
+    user: 'postgres',
+    password: 'docker',
+    database: 'proffy',
   },
   useNullAsDefault: true,
 });
